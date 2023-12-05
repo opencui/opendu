b23841dd401f43d29ee043cd9ba1db00a48be362
Sean Wu <xiaoyun.wu@gmail.com>
2023-12-04 21:16:31 -0800

    fix typo

--- src/inference/converter.py
+++ src/inference/converter.py
@@ -282,7 +282,7 @@ class Converter:
             values = []
             if self.recognizer is not None:
                 values = self.recognizer.extract_values(slot, text)
-            slot_input_dict = {"utterance": text, "values": value}
+            slot_input_dict = {"utterance": text, "values": values}
             slot_input_dict.update(module.slots[slot].to_dict())
             slot_prompts.append(self.slot_prompt(slot_input_dict))
 
