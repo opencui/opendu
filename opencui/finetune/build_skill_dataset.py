4754fdddb6b5b2e2fedf4d3621dfffb7c4eb011e
Sean Wu <xiaoyun.wu@gmail.com>
2023-12-13 18:37:24 -0800

    take out the prompt skills dataset building as they are slow

---
+++ opencui/finetune/build_skill_dataset.py
@@ -0,0 +1,48 @@
+import json
+from opencui import EmbeddingStore, PromptedFactory, build_dataset_index, JsonDatasetFactory, \
+    LugConfig, OneSkillTrainConverter, SkillTrainConverter, LayeredTrainConverter
+from opencui.core.retriever import build_desc_index, load_context_retrievers, ContextRetriever
+
+
+def skill_converter(retriever: ContextRetriever):
+    if LugConfig.skill_mode == "binary":
+        return OneSkillTrainConverter(retriever)
+    if LugConfig.skill_mode == "multiclass":
+        return SkillTrainConverter(retriever)
+    if LugConfig.skill_mode == "simple":
+        return LayeredTrainConverter(retriever)
+
+
+def build_skill_factory(output, factory):
+    # Save the things to disk first, for training we keep each module separate.
+    # Down the road, we might
+    build_desc_index(
+        factory.tag,
+        factory.schema,
+        f"{output}/index/",
+        EmbeddingStore.for_description(),
+    )
+    build_dataset_index(
+        factory.tag,
+        factory["train"],
+        f"{output}/index/",
+        EmbeddingStore.for_exemplar(),
+    )
+
+    context_retriever = load_context_retrievers({factory.tag: factory.schema}, f"{output}/index/")
+    return PromptedFactory(factory, [skill_converter(context_retriever)])
+
+
+if __name__ == "__main__":
+    path = "./datasets/sgd"
+    tag = "sgd"
+    factory = JsonDatasetFactory(path, tag)
+    prompted_factory = build_skill_factory(path, factory)
+    tags = ["train", "test", "validation"]
+    for tag in tags:
+        examples = prompted_factory[tag]
+        with open(f"{path}/prompted.{tag}.jsonl", "w") as file:
+            print(f"there are {len(examples)} examples left for {tag}.")
+            for example in examples:
+                file.write(f"{json.dumps(example)}\n")
+
--- opencui/finetune/commons.py
+++ opencui/finetune/commons.py
@@ -1,5 +1,7 @@
 import abc
 import json
+import random
+import re
 
 from abc import ABC
 from collections import defaultdict
@@ -11,9 +13,10 @@ from datasets import Dataset, load_dataset
 from llama_index.embeddings.base import BaseEmbedding
 from llama_index.schema import TextNode
 
-from opencui.core.annotation import Schema
+from opencui import Prompt, MulticlassSkillPrompts, BinarySkillPrompts, LayeredPrompts
+from opencui.core.annotation import Schema, Exemplar, ListRecognizer
 from opencui.core.config import LugConfig
-from opencui.core.retriever import HybridRetriever, create_index
+from opencui.core.retriever import HybridRetriever, create_index, ContextRetriever
 from opencui.finetune.embedding import (
     create_sentence_pair_for_description, create_sentence_pair_for_exemplars)
 
@@ -204,14 +207,14 @@ def purge_dataset(dataset, k=32):
 
 
 class JsonDatasetFactory(DatasetFactory, ABC):
-    def __init__(self, path, tag=None):
+    def __init__(self, path, tag=None, prefix=""):
         self.path = path
         schema_dict = json.load(open(f"{path}/schema.json"))
         self.schema = Schema.from_dict(schema_dict)
         files = {
-            "train": f"{self.path}/train.jsonl",
-            "test": f"{self.path}/test.jsonl",
-            "validation": f"{self.path}/validation.jsonl",
+            "train": f"{self.path}/{prefix}train.jsonl",
+            "test": f"{self.path}/{prefix}test.jsonl",
+            "validation": f"{self.path}/{prefix}validation.jsonl",
         }
         self.datasets = load_dataset('json', data_files=files)
         self.tag = tag
@@ -220,11 +223,337 @@ class JsonDatasetFactory(DatasetFactory, ABC):
         return self.datasets[item]
 
 
-if __name__ == "__main__":
-    factory = JsonDatasetFactory("./datasets/sgd/")
-    dataset = factory["train"]
-    count = 0
-    for item in dataset:
-        print(item)
-        count += 1
-    print(count)
+
+
+# This inference is responsible for convert the exemplars in the original dataset into what is needed
+# by generation fine-tuning. The assumed the columns are input and output, and we added id for debugging
+# purpose.
+class TrainConverter(ABC):
+    prompt: Prompt
+
+    @abc.abstractmethod
+    def __call__(self, item: AnnotatedExemplar, ins: list[str], outs: list[str]):
+        return
+
+
+# The slot converter need to have access to entities.
+class SlotExtractConverter(TrainConverter, ABC):
+    entities: dict[str, re.Pattern]
+
+
+# This is needed to determine the intention, intended function or skill
+# https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/
+class SkillTrainConverter(TrainConverter):
+    def __init__(self, retriever: ContextRetriever):
+        self.prompt = MulticlassSkillPrompts[LugConfig.skill_prompt]
+        self.context_retrieve = retriever
+
+    def __call__(self, batch, ins: list[str], outs: list[str]):
+        # Working on the batched dataset, with first dimension is column then index.
+        for idx, utterance in enumerate(batch["utterance"]):
+            # We assume the input is dict version of AnnotatedExemplar
+            skills, nodes = self.context_retrieve(utterance)
+            # remove the identical exemplar
+            nodes = [node for node in nodes if node.id_ != batch["id"][idx]]
+            exemplars = [
+                Exemplar(owner=node.metadata["owner"], template=node.text)
+                for node in nodes
+            ]
+            owner = batch["owner"][idx]
+
+            # How can we reduce the need for
+
+            neg_owners = [
+                node.metadata["owner"]
+                for node in nodes
+                if node.metadata["owner"] != owner
+            ]
+
+            # randomly filter one neg skills and exemplars
+            if len(neg_owners) != 0:
+                neg_owner = random.choice(neg_owners)
+                rm_neg_exemplars = [
+                    exemplar for exemplar in exemplars if exemplar.owner != neg_owner
+                ]
+                rm_neg_skills = [
+                    skill for skill in skills if skill["name"] != neg_owner
+                ]
+
+                # Without exemplars.
+                random.shuffle(rm_neg_skills)
+                input_dict = {
+                    "utterance": utterance,
+                    "examples": [],
+                    "skills": rm_neg_skills,
+                }
+                ins.append(self.prompt(input_dict))
+                outs.append(f"{json.dumps(owner)}</s>")
+
+                # With exemplars.
+                if len(rm_neg_exemplars) != 0:
+                    random.shuffle(rm_neg_exemplars)
+                    input_dict = {
+                        "utterance": utterance,
+                        "examples": rm_neg_exemplars,
+                        "skills": rm_neg_skills,
+                    }
+                    ins.append(self.prompt(input_dict))
+                    outs.append(f"{json.dumps(owner)}</s>")
+
+            # Try to filter the pos skills and exemplars
+            rm_pos_exemplars = [
+                exemplar for exemplar in exemplars if exemplar.owner != owner
+            ]
+            rm_pos_skills = [skill for skill in skills if skill["name"] != owner]
+
+            random.shuffle(rm_pos_skills)
+            input_dict = {
+                "utterance": utterance,
+                "examples": [],
+                "skills": rm_pos_skills,
+            }
+            ins.append(self.prompt(input_dict))
+            outs.append(f"{json.dumps(None)}</s>")
+
+            if len(rm_pos_exemplars) != 0:
+                random.shuffle(rm_pos_exemplars)
+                input_dict = {
+                    "utterance": utterance,
+                    "examples": rm_pos_exemplars,
+                    "skills": rm_pos_skills,
+                }
+                ins.append(self.prompt(input_dict))
+                outs.append(f"{json.dumps(None)}</s>")
+
+
+class OneSkillTrainConverter(TrainConverter):
+    def __init__(self, retriever: ContextRetriever):
+        self.prompt = BinarySkillPrompts[LugConfig.skill_prompt]
+        self.context_retrieve = retriever
+        self.neg_k = 1
+
+    def __call__(self, batch, ins: list[str], outs: list[str]):
+        # Working on the batched dataset, with first dimension is column then index.
+        for idx, utterance in enumerate(batch["utterance"]):
+            # We assume the input is dict version of AnnotatedExemplar
+            skills, nodes = self.context_retrieve(utterance)
+            # remove the identical exemplar
+            nodes = [node for node in nodes if node.id_ != batch["id"][idx]]
+            exemplars = [
+                Exemplar(owner=node.metadata["owner"], template=node.text)
+                for node in nodes
+            ]
+            owner = batch["owner"][idx]
+
+            skill_map = {}
+
+            # for the skills
+            for skill in skills:
+                input_dict = {"utterance": utterance, "examples": [], "skill": skill}
+                ins.append(self.prompt(input_dict))
+                outs.append(f"{json.dumps(owner == skill['name'])}</s>")
+                skill_map[skill["name"]] = skill
+
+            for o_exemplar in exemplars:
+                target = o_exemplar.owner
+                # Try not to have more than two examples.
+                exemplar_dicts = [
+                    {
+                        "template": exemplar.template,
+                        "target": target,
+                        "decision": target == exemplar.owner,
+                    }
+                    for exemplar in exemplars
+                ]
+
+                input_dict = {
+                    "utterance": utterance,
+                    "examples": exemplar_dicts,
+                    "skill": skill_map[target],
+                }
+                ins.append(self.prompt(input_dict))
+                outs.append(f"{json.dumps(owner == target)}</s>")
+
+
+# For this one, we first use example based prediction, and then description based prediction.
+class LayeredTrainConverter(TrainConverter):
+    def __init__(self, retriever: ContextRetriever):
+        self.desc_prompt = LayeredPrompts[LugConfig.skill_prompt][0]
+        self.example_prompt = LayeredPrompts[LugConfig.skill_prompt][1]
+        self.context_retrieve = retriever
+        self.neg_k = 1
+
+    def __call__(self, batch, ins: list[str], outs: list[str]):
+        # Working on the batched dataset, with first dimension is column then index.
+        for idx, utterance in enumerate(batch["utterance"]):
+            # We assume the input is dict version of AnnotatedExemplar
+            skills, nodes = self.context_retrieve(utterance)
+            # remove the identical exemplar
+            nodes = [node for node in nodes if node.id_ != batch["id"][idx]]
+            exemplars = [
+                Exemplar(owner=node.metadata["owner"], template=node.text)
+                for node in nodes
+            ]
+            owner = batch["owner"][idx]
+
+            skill_map = {}
+
+            # for the skills
+            for skill in skills:
+                input_dict = {"utterance": utterance, "skill": skill}
+                ins.append(self.desc_prompt(input_dict))
+                outs.append(f"{json.dumps(owner == skill['name'])}</s>")
+                skill_map[skill["name"]] = skill
+
+            for exemplar in exemplars:
+                target = exemplar.owner
+                # Try not to have more than two examples.
+                input_dict = {"utterance": utterance, "template": exemplar.template}
+                ins.append(self.example_prompt(input_dict))
+                outs.append(f"{json.dumps(owner == target)}</s>")
+
+
+#
+# This is for extractive slot value understanding.
+# For now, we only get positive example.
+class OneSlotExtractConverter(SlotExtractConverter):
+    def __init__(self, module: Schema, slot_prompt: Prompt, entities):
+        self.prompt = slot_prompt
+        self.module = module
+        self.include_negative = True
+        # First try to be efficient.
+        self.entities = entities
+        self.patterns = {}
+        for key, values in entities.items():
+            strings_to_check = list(values)
+            pattern = re.compile("|".join(map(re.escape, strings_to_check)))
+            self.patterns[key] = pattern
+
+    @staticmethod
+    def format_value(key, value=None):
+        return f"{json.dumps(value)}</s>"
+
+    def add_one_negative(self, slot_name, small_value_set):
+        if slot_name not in self.entities:
+            return
+
+        picked = None
+        candidates = self.entities[slot_name]
+
+        while picked in small_value_set:
+            picked = random.choice(candidates)
+
+        if picked is not None:
+            small_value_set.add(picked)
+
+    def __call__(self, batch, ins: list[str], outs: list[str]):
+        # We assume the input is dict version of AnnotatedExemplar
+        for idx, sarguments in enumerate(batch["arguments"]):
+            arguments = eval(sarguments)
+            utterance = batch["utterance"][idx]
+            owner = batch["owner"][idx]
+            for slot_label in self.module.skills[owner]["slots"]:
+                slot = self.module.slots[slot_label]
+                slot_name = slot["name"]
+
+                # Now we need to select the value from entities
+                # In addition to the true value, the best should be of the same type and
+                # also the occurs in the utterance but not the value.
+                values = set(
+                    ListRecognizer.find_matches(self.patterns, slot_name, utterance)
+                )
+                # Most likely we do not need to add the negatives.
+                # self.add_one_negative(slot_label, values)
+                input_dict = {"utterance": utterance}
+                input_dict.update(slot)
+                if slot_name in arguments:
+                    value = arguments[slot_name]
+                    # First without values. We assume that value is
+                    input_dict["values"] = []
+                    ins.append(self.prompt(input_dict))
+                    if len(value) == 1:
+                        outs.append(
+                            self.format_value(slot_name, arguments[slot_name][0])
+                        )
+                    else:
+                        outs.append(self.format_value(slot_name, arguments[slot_name]))
+                    # then with values.
+                    input_dict["values"] = values
+                    ins.append(self.prompt(input_dict))
+                    if len(value) == 1:
+                        outs.append(
+                            self.format_value(slot_name, arguments[slot_name][0])
+                        )
+                    else:
+                        outs.append(self.format_value(slot_name, arguments[slot_name]))
+                else:
+                    input_dict["values"] = []
+                    if self.include_negative:
+                        ins.append(self.prompt(input_dict))
+                        outs.append(self.format_value(slot_name, None))
+
+
+# We need to handle many different use case here: premise is what user said, and hypothesis is what we want to know.
+class NliConverter(TrainConverter, ABC):
+    def __init__(self, prompt):
+        self.prompt = prompt
+        self.labels = ["entailment", "neutral", "contradiction"]
+
+    def __call__(self, batch, ins: list[str], outs: list[str]):
+        # We assume the input is dict version of AnnotatedExemplar
+        for idx, premise in enumerate(batch["premise"]):
+            hypothesis = batch["hypothesis"][idx]
+            label = self.labels[int(batch["label"][idx])]
+            input_dict = {"premise": premise, "hypothesis": hypothesis}
+            ins.append(self.prompt(input_dict))
+            outs.append(f"{label}</s>")
+
+
+# This inference is needed for cases where users' utterance is response to bot's prompt questions, and
+# needs the abstractive understanding instead of extractive understanding.
+# This is needed to determine the intention, intended function or skill
+# class BooleanConverter
+@dataclass
+class PromptedFactory(DatasetFactory):
+    __metaclass__ = abc.ABCMeta
+    skill_columns = [
+        "id",
+        "utterance",
+        "template",
+        "owner",
+        "arguments",
+        "expectations",
+    ]
+
+    def __init__(
+        self,
+        dsf: DatasetFactory,
+        convert: list[TrainConverter],
+        unused_columns=skill_columns,
+    ):
+        self.creator = dsf
+        self.converters: list[TrainConverter] = convert
+        self.columns = unused_columns
+
+    def extra_tokens(self):
+        return list(
+            set(
+                [
+                    token
+                    for converter in self.converters
+                    for token in converter.prompt.extra_tokens
+                ]
+            )
+        )
+
+    def convert_one(self, item):
+        ins = []
+        outs = []
+        for convert in self.converters:
+            convert(item, ins, outs)
+        assert len(ins) == len(outs)
+        return {"input": ins, "output": outs}
+
+    def __getitem__(self, split: str) -> Dataset:
+        dataset = self.creator[split]
+        return dataset.map(self.convert_one, batched=True, remove_columns=self.columns)
