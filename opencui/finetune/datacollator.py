75ceedb189b8d9777d77f4f0a8629a11d9b878e6
Sean Wu <xiaoyun.wu@gmail.com>
2023-12-24 10:16:35 -0800

    more refactor

---
+++ opencui/finetune/datacollator.py
@@ -0,0 +1,79 @@
+import copy
+import dataclasses
+
+import torch
+import transformers
+from typing import Dict, Optional, Sequence, List, Tuple
+from torch.nn.utils.rnn import pad_sequence
+
+IGNORE_INDEX = -100
+
+
+# This is used for adapting CausalLM to seq to seq task in a conditional sense.
+# So loss on the input does not count here.
+@dataclasses.dataclass
+class DataCollatorForCausalLM(object):
+    tokenizer: transformers.PreTrainedTokenizer
+    source_max_len: int
+    target_max_len: int
+    train_on_source: bool
+    predict_with_generate: bool
+
+    def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
+        # Extract elements
+        sources = [f"{example['input']}" for example in instances]
+        targets = [
+            f"{example['output']} {self.tokenizer.eos_token}" for example in instances
+        ]
+
+        # Tokenize
+        tokenized_sources_with_prompt = self.tokenizer(
+            sources,
+            max_length=self.source_max_len,
+            truncation=True,
+            add_special_tokens=False,
+        )
+        tokenized_targets = self.tokenizer(
+            targets,
+            max_length=self.target_max_len,
+            truncation=True,
+            add_special_tokens=False,
+        )
+        # Build the input and labels for causal LM
+        input_ids = []
+        labels = []
+        for tokenized_source, tokenized_target in zip(
+                tokenized_sources_with_prompt["input_ids"], tokenized_targets["input_ids"]
+        ):
+            if not self.predict_with_generate:
+                input_ids.append(torch.tensor(tokenized_source + tokenized_target))
+                # This skip the source.
+                if not self.train_on_source:
+                    labels.append(
+                        torch.tensor(
+                            [IGNORE_INDEX for _ in range(len(tokenized_source))]
+                            + copy.deepcopy(tokenized_target)
+                        )
+                    )
+                else:
+                    labels.append(
+                        torch.tensor(copy.deepcopy(tokenized_source + tokenized_target))
+                    )
+            else:
+                input_ids.append(torch.tensor(tokenized_source))
+        # Apply padding
+        input_ids = pad_sequence(
+            input_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id
+        )
+        labels = (
+            pad_sequence(labels, batch_first=True, padding_value=IGNORE_INDEX)
+            if not self.predict_with_generate
+            else None
+        )
+        data_dict = {
+            "input_ids": input_ids,
+            "attention_mask": input_ids.ne(self.tokenizer.pad_token_id),
+        }
+        if labels is not None:
+            data_dict["labels"] = labels
+        return data_dict
--- opencui/finetune/t2t.py
+++ opencui/finetune/t2t.py
@@ -16,14 +16,14 @@ import torch
 import transformers
 from datasets import Dataset, concatenate_datasets, load_dataset
 from peft import LoraConfig, get_peft_model, TaskType, PrefixTuningConfig
-from torch.nn.utils.rnn import pad_sequence
+
 from transformers import (AutoModelForCausalLM, AutoTokenizer, Seq2SeqTrainer, set_seed,
                           DataCollatorForSeq2Seq, AutoModelForSeq2SeqLM)
 from opencui.core.prompt import (ExtractiveSlotPrompts, NliPrompts)
 from opencui.core.special_tokens import SpecialTokens
 from opencui.finetune.commons import (MappedDatasetDict, collect_slot_values, JsonDatasetFactory,
-                                      OneSlotExtractConverter, PromptedFactory, NliConverter)
-from opencui.finetune.load_trainingset import load_training_dataset
+                                      OneSlotExtractConverter, PromptedFactory, NliConverter, load_training_dataset)
+from opencui.finetune.datacollator import DataCollatorForCausalLM
 
 logger = logging.getLogger(__name__)
 
@@ -314,70 +314,7 @@ def smart_tokenizer_and_embedding_resize(
 
 
 @dataclass
-class DataCollatorForCausalLM(object):
-    tokenizer: transformers.PreTrainedTokenizer
-    source_max_len: int
-    target_max_len: int
-    train_on_source: bool
-    predict_with_generate: bool
 
-    def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
-        # Extract elements
-        sources = [f"{example['input']}" for example in instances]
-        targets = [
-            f"{example['output']} {self.tokenizer.eos_token}" for example in instances
-        ]
-
-        # Tokenize
-        tokenized_sources_with_prompt = self.tokenizer(
-            sources,
-            max_length=self.source_max_len,
-            truncation=True,
-            add_special_tokens=False,
-        )
-        tokenized_targets = self.tokenizer(
-            targets,
-            max_length=self.target_max_len,
-            truncation=True,
-            add_special_tokens=False,
-        )
-        # Build the input and labels for causal LM
-        input_ids = []
-        labels = []
-        for tokenized_source, tokenized_target in zip(
-            tokenized_sources_with_prompt["input_ids"], tokenized_targets["input_ids"]
-        ):
-            if not self.predict_with_generate:
-                input_ids.append(torch.tensor(tokenized_source + tokenized_target))
-                if not self.train_on_source:
-                    labels.append(
-                        torch.tensor(
-                            [IGNORE_INDEX for _ in range(len(tokenized_source))]
-                            + copy.deepcopy(tokenized_target)
-                        )
-                    )
-                else:
-                    labels.append(
-                        torch.tensor(copy.deepcopy(tokenized_source + tokenized_target))
-                    )
-            else:
-                input_ids.append(torch.tensor(tokenized_source))
-        # Apply padding
-        input_ids = pad_sequence(
-            input_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id
-        )
-        labels = (
-            pad_sequence(labels, batch_first=True, padding_value=IGNORE_INDEX)
-            if not self.predict_with_generate
-            else None
-        )
-        data_dict = {
-            "input_ids": input_ids,
-            "attention_mask": input_ids.ne(self.tokenizer.pad_token_id),
-        }
-        if labels is not None:
-            data_dict["labels"] = labels
-        return data_dict
 
 
 class DatasetAdaptor:
@@ -524,6 +461,7 @@ def train():
     if completed_training:
         print("Detected that training was already completed!")
 
+    print(f"predict_with_generate is: {args.predict_with_generate}")
 
     print("loaded model")
     set_seed(args.seed)
