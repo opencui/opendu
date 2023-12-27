86a87f2f28078adbb1b2d6aa34af33e5d1968358
Sean Wu <xiaoyun.wu@gmail.com>
2023-12-26 19:16:19 -0800

    use a strong sense of implies

--- opencui/inference/converter.py
+++ opencui/inference/converter.py
@@ -298,11 +298,13 @@ class ISkillConverter(SkillConverter, ABC):
     def update(preds, truth, counts, skill_prompts, skill_outputs):
         pairs = zip(preds, truth)
         for index, pair in enumerate(pairs):
-            if pair[0] != pair[1]:
+            if pair[0] != pair[1] and pair[1] is not None:
                 print(f"{skill_prompts[index]} {skill_outputs[index]}, not correct.")
 
         pairs = zip(preds, truth)
         for pair in pairs:
+            if pair[1] is None:
+                continue
             index = 2 if pair[0] else 0
             index += 1 if pair[1] else 0
             counts[index] += 1
