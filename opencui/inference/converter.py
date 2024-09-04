479fd5f78255b646cd7e96054bfd82080af82d63
Sean Wu <xioayun.wu@gmail.com>
2024-09-04 16:40:12 -0700

    small steps

--- opencui/inference/converter.py
+++ opencui/inference/converter.py
@@ -2,204 +2,22 @@ import json
 import re
 from abc import ABC, abstractmethod
 from collections import defaultdict
-from enum import Enum
 
-import torch
-from peft import PeftConfig, PeftModel
-from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig, AutoModelForSeq2SeqLM, AutoConfig
 
-from opencui import ModelType
 from opencui.core.annotation import (CamelToSnake, DialogExpectation, EntityMetas, Exemplar, FrameValue, ListRecognizer,
                                      OwnerMode, ExactMatcher, get_value)
 from opencui.core.config import LugConfig
-from opencui.core.prompt import (ExtractiveSlotPrompts, YniPrompts, DescriptionPrompts, ExemplarPrompts)
+from opencui.core.pybars_prompt import (ExtractiveSlotPrompts, YniPrompts, DescriptionPrompts, ExemplarPrompts)
 from opencui.core.retriever import (ContextRetriever, load_context_retrievers)
+from opencui.inference.llm_generator import GenerateMode, Generator
 from opencui.inference.schema_parser import load_all_from_directory
-
-# The modes that we will support.
-GenerateMode = Enum("GenerateMode", ["desc", "exemplar", "extractive", "nli"])
-GeneratorType = Enum("Generator", ["FftGenerator", "LoraGenerator"])
-YesNoResult = Enum("YesNoResult", ["Affirmative", "Negative", "Indifferent", "Irrelevant"])
-
-# In case you are curious about decoding: https://huggingface.co/blog/how-to-generate
-# We are not interested in the variance, so we do not do sampling not beam search.
-#
-# Here are the work that client have to do.
-# For now, assume single module. We can worry about multiple module down the road.
-# 1. use description and exemplars to find built the prompt for the skill prompt.
-# 2. use skill and recognizer to build prompt for slot.
-# 3. stitch together the result.
-# Generator is responsible for low level things, we will have two different implementation
-# local/s-lora. Converter is built on top of generator.
-class Generator(ABC):
-    generator = None
-
-    @staticmethod
-    def build():
-        if GeneratorType[LugConfig.get().generator] == GeneratorType.FftGenerator and Generator.generator is None:
-            Generator.generator = FftGenerator()
-        if GeneratorType[LugConfig.get().generator] == GeneratorType.LoraGenerator and Generator.generator is None:
-            Generator.generator = LoraGenerator()
-        return Generator.generator
-
-    @staticmethod
-    def get_model_type(model_name_or_path):
-        config = AutoConfig.from_pretrained(model_name_or_path)
-        return config.model_type
-
-    @staticmethod
-    def from_pretrained(*args, **kwargs):
-        config = AutoConfig.from_pretrained(args[0])
-        # Check the model type
-        print(f"loading model: {args[0]} with type: {config.model_type}")
-        if ModelType.normalize(config.model_type) == ModelType.t5:
-            return AutoModelForSeq2SeqLM.from_pretrained(*args, **kwargs)
-        if ModelType.normalize(config.model_type) == ModelType.gpt:
-            return AutoModelForCausalLM.from_pretrained(*args, **kwargs)
-
-    @abstractmethod
-    def generate(self, input_texts: list[str], mode: GenerateMode = None):
-        pass
-
-    def process_return(self, outputs: list[str], input_texts: list[str]):
-        if ModelType.normalize(self.model_type) == ModelType.t5:
-            return outputs
-        if ModelType.normalize(self.model_type) == ModelType.gpt:
-            return [output[len(input_texts[index]):] for index, output in enumerate(outputs)]
-
-
-# This should be desc/exemplar based.
-class LoraGenerator(Generator, ABC):
-    def __init__(self):
-        parts = LugConfig.get().skill_model.split("/")
-
-        desc_model = f"{parts[0]}/desc-{parts[1]}"
-        exemplar_model = f"{parts[0]}/exemplar-{parts[1]}"
-
-        skill_config = PeftConfig.from_pretrained(desc_model)
-
-        model_path = skill_config.base_model_name_or_path
-
-        # Is this the right place to clean cache.
-        torch.cuda.empty_cache()
-
-        base_model = Generator.from_pretrained(
-            skill_config.base_model_name_or_path,
-            return_dict=True,
-            device_map=LugConfig.get().llm_device,
-            trust_remote_code=True,
-            torch_dtype=torch.bfloat16
-        )
-
-        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
-        self.tokenizer.pad_token = self.tokenizer.eos_token
-        self.tokenizer.padding_side = "left"
-        self.models = {}
-
-        self.lora_model = PeftModel.from_pretrained(
-            base_model, desc_model, adapter_name=GenerateMode.desc.name)
-
-        self.lora_model.load_adapter(
-            exemplar_model, adapter_name=GenerateMode.exemplar.name)
-
-        self.lora_model.load_adapter(
-            LugConfig.get().extractive_slot_model, adapter_name=GenerateMode.extractive.name)
-
-        if LugConfig.get().nli_model != "":
-            self.lora_model.load_adapter(LugConfig.get().nli_model, adapter_name=GenerateMode.nli.name)
-
-        # Move to device
-        self.lora_model.to(LugConfig.get().llm_device)
-        self.lora_model.eval()
-
-    def generate(self, input_texts: list[str], mode: GenerateMode):
-        self.lora_model.set_adapter(mode.name)
-        encoding = self.tokenizer(
-            input_texts, padding=True, return_tensors="pt"
-        ).to(LugConfig.get().llm_device)
-
-        with torch.no_grad():
-            peft_outputs = self.lora_model.generate(
-                input_ids=encoding.input_ids,
-                generation_config=GenerationConfig(
-                    max_new_tokens=32,
-                    pad_token_id=self.tokenizer.eos_token_id,
-                    eos_token_id=self.tokenizer.eos_token_id,
-                    attention_mask=encoding.attention_mask,
-                    do_sample=False,
-                    repetition_penalty=1.2,
-                    num_return_sequences=1,
-                ),
-            )
-
-        results = self.tokenizer.batch_decode(peft_outputs, skip_special_tokens=True)
-        return Generator.process_return(results, input_texts)
-
-
-# Full finetuned generator
-class FftGenerator(Generator, ABC):
-    def __init__(self):
-        # Is this the right place to clean cache.
-        torch.cuda.empty_cache()
-        self.model = Generator.from_pretrained(
-            LugConfig.get().model,
-            return_dict=True,
-            device_map=LugConfig.get().llm_device,
-            trust_remote_code=True,
-            torch_dtype=torch.bfloat16
-        )
-        self.model_type = Generator.get_model_type(LugConfig.get().model)
-        self.model.eval()
-        self.tokenizer = AutoTokenizer.from_pretrained(LugConfig.get().model)
-        self.tokenizer.pad_token = self.tokenizer.eos_token
-        self.tokenizer.padding_side = "left"
-
-        # Move to device
-        self.model.to(LugConfig.get().llm_device)
-        self.model.eval()
-
-    def generate(self, input_texts: list[str], mode: GenerateMode):
-        # The tokenizer can not handle empty list, so we safeguard that.
-        if len(input_texts) == 0:
-            return []
-
-        encoding = self.tokenizer(
-            input_texts, padding=True, truncation=True, return_tensors="pt"
-        ).to(LugConfig.get().llm_device)
-
-        with torch.no_grad():
-            outputs = self.model.generate(
-                input_ids=encoding.input_ids,
-                generation_config=GenerationConfig(
-                    max_new_tokens=32,
-                    pad_token_id=self.tokenizer.eos_token_id,
-                    bos_token_id=self.tokenizer.bos_token_id,
-                    eos_token_id=self.tokenizer.eos_token_id,
-                    attention_mask=encoding.attention_mask,
-                    do_sample=False,
-                    repetition_penalty=1.2,
-                    num_return_sequences=1,
-                ),
-            )
-        results = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
-        return self.process_return(results, input_texts)
+from opencui.utils.json_tools import parse_json_from_string
 
 
 class SkillConverter(ABC):
     @abstractmethod
-    def get_skills(self, text) -> list[str]:
+    def get_skills(self, text, expectations=None, debug=False):
         pass
-
-    @abstractmethod
-    def grade(self, text, owner, owner_mode, counts_dict):
-        pass
-
-
-def parse_json_from_string(text, default=None):
-    try:
-        return json.loads(text)
-    except ValueError as e:
-        return default
 
 
 # This is used to pick the owner by first accumulate on the exemplars by weight 2
@@ -241,7 +59,9 @@ class SingleOwnerPicker:
         return None if len(pairs) == 0 else pairs[0][0]
 
 
-class ISkillConverter(SkillConverter, ABC):
+# This converter is based on the idea that if user utterance is deemed as equivalent to exemplar, and
+# We use owner on these matched exemplar to decide which skills user implies.
+class ExemplarConverter(SkillConverter, ABC):
     def __init__(self, retriever: ContextRetriever, generator):
         self.retrieve = retriever
         self.generator = generator
@@ -324,7 +144,7 @@ class ISkillConverter(SkillConverter, ABC):
             }
             infos.append(item)
 
-    def get_full_skills(self, text, expectations, debug=False):
+    def get_full_skills(self, text, expectations=None, debug=False):
         print(f"parse for skill: {text} with {expectations}")
         # For now, we only pick one skill
         picker = SingleOwnerPicker(expectations)
@@ -371,9 +191,6 @@ class ISkillConverter(SkillConverter, ABC):
         label = picker.decide()
         return label, list(map(node_to_exemplar, exemplar_nodes)), debug_infos
 
-    def get_skills(self, text):
-        label, node = self.get_full_skills(text)
-        return label
 
     @staticmethod
     def update(preds, truth, counts, skill_prompts, skill_outputs, output=True):
@@ -449,7 +266,6 @@ class ISkillConverter(SkillConverter, ABC):
         self.update(desc_preds, desc_truth, count_dict["desc"], desc_prompts, desc_outputs, debug_output)
 
 
-
 def node_to_exemplar(node):
     meta = node.metadata
     result = {
@@ -488,14 +304,14 @@ class Converter:
         self.bracket_match = re.compile(r"\[([^]]*)\]")
         self.skill_converter = None
 
-        self.skill_converter = ISkillConverter(retriever, self.generator)
+        self.skill_converter = ExemplarConverter(retriever, self.generator)
         self.yni_results = {"Affirmative", "Negative", "Indifferent", "Irrelevant" }
 
 
     # Reference implementation for function calling.
     def understand(self, text: str) -> FrameValue:
         # low level get skill.
-        func_name = self.skill_converter.get_skills(text)
+        func_name, _ = self.skill_converter.get_full_skills(text)
 
         if not self.with_arguments:
             return FrameValue(name=func_name, arguments={})
