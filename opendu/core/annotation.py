eb099f009891f2610c44c58bca30c06d2c34526d
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2025-03-08 03:02:41 +0000

    continue to simplify

--- opendu/core/annotation.py
+++ opendu/core/annotation.py
@@ -80,49 +80,6 @@ class Schema(BaseModel):
         return res
 
 
-OwnerMode = Enum('OwnerMode', ["normal", "extended", "negative"])
-
-
-# This considers the match under the exact sense.
-class ExactMatcher:
-    @staticmethod
-    def agree(owner, owner_mode, target, target_mode):
-        label_match = owner == target
-        if not label_match:
-            return label_match
-
-        # We should not use this example.
-        if OwnerMode[owner_mode] != OwnerMode.normal and OwnerMode[target_mode] != OwnerMode.normal:
-            return None
-
-        # now we have match, but mode does not match.
-        return OwnerMode[owner_mode] == OwnerMode.normal and OwnerMode[target_mode] == OwnerMode.normal
-
-    @staticmethod
-    def match(owner, target, mode_in_str):
-        return owner == target and OwnerMode[mode_in_str] == OwnerMode.normal
-
-    @staticmethod
-    def is_good_mode(mode_in_str):
-        return OwnerMode[mode_in_str] == OwnerMode.normal
-
-
-class FrameValue(BaseModel):
-    name: str
-    arguments: TypedDict
-
-
-class FrameState(BaseModel):
-    frame: str
-    slot: str
-    slotType: str
-
-
-# How to present context is strictly state tracking implementation dependent.
-class DialogExpectation(BaseModel):
-    context: list[FrameState]
-
-
 class EntityInstance(BaseModel):
     label: str = Field(description="the canonical form of the instance")
     expressions: List[str] = Field(
@@ -193,6 +150,7 @@ def get_value(item, key, value=None):
         return item[key]
     except:
         return value
+
 
 
 #
