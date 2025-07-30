76cacbc886cb70cf1319c6bf56f93200efeff15e
xiaoyun wu <xiaoyun.wu@gmail.com>
2025-07-29 22:57:27 -0700

    update configure

--- opendu/core/config.py
+++ opendu/core/config.py
@@ -39,59 +39,12 @@ class RauConfig:
         return RauConfig._instance
 
 
-# This is used to
-class BaseConfig(BaseModel):
+
+
+class BcSsYniFullConfig(BaseModel):
     embedding_device: str = DEVICE
-    #embedding_model: str = "BAAI/bge-base-en-v1.5"
-    #embedding_model: str = "dunzhang/stella_en_400M_v5"
-    embedding_model: str = "intfloat/multilingual-e5-large-instruct"
-    #embedding_model: str = "HIT-TMG/KaLM-embedding-multilingual-mini-instruct-v1.5"
+    embedding_model: str = "Qwen/Qwen3-Embedding-0.6B"
 
-    # We might not want to touch this, without rerun find_k
-    desc_retrieve_topk: int = 8
-    exemplar_retrieve_topk: int = 8
-    exemplar_retrieve_arity: int = 8
-
-    skill_arity: int = 1
-    llm_device: str = DEVICE
-
-    # The correct decomposition is type (skill, slot, yni), task, and prompt.
-    skill_modes: list = field(default_factory=lambda: ["both"])
-
-    skill_task: str =  "id_mc"
-    slot_task: str = "sf_se"
-
-    trn_skill_prompt: str = "id_mc_full"
-    trn_slot_prompt: str = "sf_se_full"
-
-    skill_desc_prompt: str = "skill-desc-structural"
-    skill_prompt: str = "skill-knn-structural"
-    slot_prompt: str = "slot_qa_structural"
-    yni_prompt: str = "yni-default"
-    bool_prompt: str = "plain"
-
-    eval_mode: bool = True
-
-    # We will append instance.desc/instance.exemplar to this.
-    generator: str = "FftGenerator"
-    model: str = "OpenCUI/dug-t5base-0.1"
-
-    # When we use one lora for each task, which we should not.
-    skill_model: str = ""
-    extractive_slot_model: str = ""
-    nli_model: str = ""
-    converter_debug: bool = True
-
-
-
-class BcSsYniConfig(BaseModel):
-
-
-    embedding_device: str = DEVICE
-    #embedding_model: str = "BAAI/bge-base-en-v1.5"
-    #embedding_model: str = "dunzhang/stella_en_400M_v5"
-    embedding_model: str = "intfloat/multilingual-e5-large-instruct"
-    #embedding_model: str = "HIT-TMG/KaLM-embedding-multilingual-mini-instruct-v1.5"
 
     # We might not want to touch this, without rerun find_k
     desc_retrieve_topk: int = 8
@@ -110,14 +63,14 @@ class BcSsYniConfig(BaseModel):
     prompt: dict = Field(default_factory=lambda: {"id_bc": "id_bc_literal", "yni": "yni_default"})
 
     # All task should share the same base model
-    base_model: str = "Qwen/Qwen2.5-7B-Instruct"
+    base_model: str = "Qwen/Qwen3-4B"
     eval_mode: bool = True
 
     # We will append instance.desc/instance.exemplar to this.
     generator: str = "BcSsYniGenerator"
 
     # When we use one lora for each task, which we should not.
-    id_bc_model: str = "OpenCUI/IdBc-Qwen2.5-7B-Instruct-0.1"
-    sf_ss_model: str = "OpenCUI/SfSs-Qwen2.5-7B-Instruct-0.1"
-    yni_model: str = "OpenCUI/Yni-Qwen2.5-7B-Instruct-0.1"
+    #id_bc_model: str = "OpenCUI/IdBc-Qwen2.5-7B-Instruct-0.1"
+    #sf_ss_model: str = "OpenCUI/SfSs-Qwen2.5-7B-Instruct-0.1"
+    # yni_model: str = "OpenCUI/Yni-Qwen2.5-7B-Instruct-0.1"
     converter_debug: bool = True
