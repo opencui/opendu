d8113d73ca23ef4ac2a41152e106bb32eadfc7b0
stephen <xiaoyun.wu@gmail.com>
2025-07-31 17:13:36 -0700

    continue on slot filling

--- opendu/inference/parser.py
+++ opendu/inference/parser.py
@@ -55,57 +55,6 @@ class Parser:
         self.skill_converter = KnnIntentDetector(retriever, self.generator)
         self.yni_results = {"Affirmative", "Negative", "Indifferent", "Irrelevant" }
 
-
-    # Reference implementation for function calling.
-    def understand(self, text: str) -> FrameValue:
-        # low level get skill.
-        func_name, _ = self.skill_converter.detect_intents(text)
-
-        if not self.with_arguments:
-            return FrameValue(name=func_name, arguments={})
-
-        # We assume the function_name is global unique for now. From UI perspective, I think
-        module = self.retrieve.module
-        slot_labels_of_func = module.skills[func_name]["slots"]
-
-        # Then we need to create the prompt for the parameters.
-        slot_prompts = []
-        for slot in slot_labels_of_func:
-            values = []
-            if self.recognizer is not None:
-                values = self.recognizer.extract_values(slot, text)
-            slot_input_dict = {"utterance": text, "values": values}
-            slot_input_dict.update(module.slots[slot])
-            slot_prompts.append(self.slot_prompt(slot_input_dict))
-
-        if RauConfig.get().converter_debug:
-            print(json.dumps(slot_prompts, indent=2))
-        slot_outputs = self.generator.generate(slot_prompts, GenerateMode.extractive)
-
-        if RauConfig.get().converter_debug:
-            print(json.dumps(slot_outputs, indent=2))
-
-        slot_values = [parse_json_from_string(seq) for seq in slot_outputs]
-        slot_values = dict(zip(slot_labels_of_func, slot_values))
-        slot_values = {
-            key: value for key, value in slot_values.items() if value is not None
-        }
-
-        final_name = func_name
-        if module.backward is not None:
-            final_name = module.backward[func_name]
-
-        return FrameValue(name=final_name, arguments=slot_values)
-
-
-    def debug(self, utterance, expectations):
-        _, _, debugs = self.skill_converter.detect_intents(utterance, expectations, True)
-        # For now, we assume single intent.
-
-        # TODO: figure out how to handle the multi intention utterance.
-        return debugs
-
-
     def detect_triggerables(self, utterance, expectations, candidates = {}, debug=False):
         func_name, evidence, _ = self.skill_converter.detect_intents(utterance, expectations, debug)
         # For now, we assume single intent.
