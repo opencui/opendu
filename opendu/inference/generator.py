71517c9dd3a6b49f4d2fb619d8ef1fdad71d275e
xiaoyun wu <xiaoyun.wu@gmail.com>
2025-08-02 19:58:16 -0700

    initial yni

--- opendu/inference/generator.py
+++ opendu/inference/generator.py
@@ -4,22 +4,15 @@
 # This source code is licensed under the BeThere AI license.
 # See LICENSE file in the project root for full license information.
 from abc import ABC, abstractmethod
-from enum import Enum
-
-import torch
-from peft import PeftConfig, PeftModel
-from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig, AutoModelForSeq2SeqLM, AutoConfig
-
-from opendu import ModelType
-from opendu.core.config import RauConfig
+from opendu.core.config import GeneratorType, RauConfig
 from vllm import LLM, SamplingParams
 from pydantic import BaseModel, Field
 from vllm.sampling_params import GuidedDecodingParams
 
-# The modes that we will support.
-# GeneratorType = Enum("Generator", ["FftGenerator", "LoraGenerator"])
-GenerateMode = Enum("GenerateMode", ["desc", "exemplar", "extractive", "nli"])
-
+# This is expectation for the output of the generator.
+# different implementation may need to translate this into different format.
+# For example, the vllm generator will use this to set the sampling parameters.
+# The vllm generator will use this to set the sampling parameters.
 class OutputExpectation(BaseModel):
     temperature: float = Field(default=0.0, description="Temperature for the decoding process.")
     top_p: float = Field(default=0.9, description="Top-p, e.g., 'The answer is {answer}'.")
@@ -27,6 +20,7 @@ class OutputExpectation(BaseModel):
     repetition_penalty: float = Field(default=1.0, description="Repetition penalty for the decoding process.")
     choices: list[str] = Field(default_factory=list, description="List of expected outputs from the model.")
 
+#
 # In case you are curious about decoding: https://huggingface.co/blog/how-to-generate
 # We are not interested in the variance, so we do not do sampling not beam search.
 #
@@ -37,7 +31,17 @@ class OutputExpectation(BaseModel):
 # 3. stitch together the result.
 # Generator is responsible for low level things, we will have two different implementation
 # local/s-lora. Converter is built on top of generator.
+# This is singlton instance. (Later we can separate the llm into a different process)
+#
 class Generator(ABC):
+    generator = None
+    
+    @staticmethod
+    def get():
+        if RauConfig.get().generator == GeneratorType.FftGenerator and Generator.generator is None:
+            Generator.generator = FftVllmGenerator(RauConfig.get().base_model)
+        return Generator.generator
+    
     @abstractmethod
     def generate(self, input_texts: list[str], expectation: OutputExpectation):
         pass
@@ -46,6 +50,7 @@ class Generator(ABC):
         return outputs
 
 
+# The caller need to set the output git diexpectation.
 class FftVllmGenerator(Generator):
     def __init__(self, model: str):
         self.model = LLM(model=model, enable_prefix_caching=True)
@@ -68,8 +73,6 @@ class FftVllmGenerator(Generator):
 
         outputs = self.model.generate(input_texts, sampling_params=samplingParams)
         return self.process_return(outputs, input_texts)
-
-
 
 
 if __name__ == "__main__":
